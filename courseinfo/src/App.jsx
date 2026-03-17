55f0830c18382d813eb51bd1ece49bc1289d9eb9
Jim Mohrle <jpmohrle@outlook.com>
2026-03-17 14:20:56 -0400

    eat(courseinfo): course information step 7 (ex-2.2)

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,9 +1,17 @@
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total
+        total={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises +
+          course.parts[3].exercises
+        }
+      />
     </div>
   )
 }
@@ -15,6 +23,7 @@ const Content = (props) => (
     <Part part={props.parts[0]} />
     <Part part={props.parts[1]} />
     <Part part={props.parts[2]} />
+    <Part part={props.parts[3]} />
   </div>
 )
 
@@ -23,6 +32,8 @@ const Part = (props) => (
     {props.part.name} {props.part.exercises}
   </p>
 )
+
+const Total = (props) => <p><b>total of {props.total} exercises</b></p>
 
 const App = () => {
   const course = {
@@ -43,6 +54,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
